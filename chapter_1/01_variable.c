d87151e5ec66756be8715e91763bd051e687f0a3
Aminourislam <mdamenourislam@gmail.com>
2026-07-28 22:31:43 +0600

    a little bit change

--- chapter_1/01_variable.c
+++ chapter_1/01_variable.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(){
     int a; //variable declaration
-    a = 3;
-    printf("%d", a);
+    a = 3; // variable inilation
+    printf("%d", a);// %d-> datatype spacifier
     printf(" The output of this is %d", a );
     return 0;
 } 
