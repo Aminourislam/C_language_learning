ed2a54cab60d9228a53a8a5cccbc0a53e3a2b671
Aminourislam <mdamenourislam@gmail.com>
2026-08-17 18:22:32 +0600

    a little bit reorganized code

--- chapter_3/if-else-switch/05_Logical_oparators.c
+++ chapter_3/if-else-switch/05_Logical_oparators.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     printf("Realational oparators:\n and --> &&\n or --> ||\n not --> ! \n");
 
     // and mane *
@@ -8,13 +9,11 @@ int main(){
     // not mane ultai deoya
     int a = 0, b = 0;
 
-
-
     printf("When a = 0 and b = 0  \n");
     printf("Print the value of a && b is %d \n", a && b);
     printf("Print the value of a || b is %d \n", a || b);
 
-    printf("a = %d \n",  a);
+    printf("a = %d \n", a);
     printf("The value of not(a) is %d\n\n", !a);
 
     a = !a;
@@ -22,25 +21,23 @@ int main(){
     printf("Print the value of a && b is %d \n", a && b);
     printf("Print the value of a || b is %d \n \n", a || b);
 
-
     printf("When a = 0 and b = 1  \n");
     a = 0;
     b = 1;
     printf("Print the value of a and / &&  b is %d \n", a && b);
     printf("Print the value of a or / || b is %d \n\n", a || b);
 
-
     printf("Useags of this oparators: \n");
     // a = 1
     // b = 1
-    a = a+1;
+    a = a + 1;
 
     if (a && b)
-    {   
+    {
         printf("Both are true! \n");
     }
-   
-    // er likthe hoito 
+
+    // er likthe hoito
 
     if (a)
     {
@@ -48,10 +45,7 @@ int main(){
         {
             printf("Both are ture! \n");
         }
-        
     }
-    
-    
 
     return 0;
 }
