d29eac3409b9400de587e3ea797105ac9723a5c2
Aminourislam <mdamenourislam@gmail.com>
2026-08-20 01:27:29 +0600

    Cloneing repo in local and initial ..

--- chapter_4/do-while-1.c
+++ chapter_4/do-while-1.c
@@ -1,16 +1,17 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     int i, num, sum;
-    i=1;
-    sum=0;
+    i = 1;
+    sum = 0;
     printf("Enter the a number = ");
     scanf("%d", &num);
     do
     {
         sum = sum + i;
         i++;
-    } while (i<=num);
+    } while (i <= num);
     printf("The sum of (1-%d) is = %d \n", num, sum);
 
-return 0;
+    return 0;
 }
